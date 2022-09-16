d7265abaeba009412b80c3748ad88b4901b20152
genaroibc <bonavitaismael@gmail.com>
2022-09-16 15:58:41 -0300

    feat: implement images slides

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,71 +1,97 @@
+import { useEffect } from "react";
 import styled from "styled-components";
 import { STFlexCont } from "./shared/STFlexCont";
+import { STImg } from "./shared/STImg";
+import { STPicture } from "./shared/STPicture";
 
-const STCarrousel = styled.div`
-  margin: 1rem auto;
-  background-color: var(--secondary-color);
+const SLIDER_CLASS = "slider-visible";
+
+const slidesConfig = [
+  {
+    id: "proj-1",
+    imgUrl: "https://placeimg.com/600/600/nature",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus consequatur minus in reprehenderit, fugiat expedita reiciendis saepe, consectetur consequuntur libero ipsum repellat ab laboriosam autem animi beatae dicta, nulla voluptatum?",
+  },
+  {
+    id: "proj-2",
+    imgUrl: "/img/tictactoe.png",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus consequatur minus in reprehenderit, fugiat expedita reiciendis saepe, consectetur consequuntur libero ipsum repellat ab laboriosam autem animi beatae dicta, nulla voluptatum?",
+  },
+  {
+    id: "proj-3",
+    imgUrl: "https://placeimg.com/600/600/arch",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus consequatur minus in reprehenderit, fugiat expedita reiciendis saepe, consectetur consequuntur libero ipsum repellat ab laboriosam autem animi beatae dicta, nulla voluptatum?",
+  },
+];
+
+const StNav = styled.nav`
+  width: 100%;
+  max-width: 300px;
+  background-color: var(--terciary-color);
+  display: flex;
+  justify-content: space-evenly;
+  margin: auto;
+
+  button {
+    font-size: 1rem;
+    text-align: center;
+    border-radius: 50%;
+  }
 `;
 
-const STDescription = styled.div`
-  margin: 1rem auto;
+const STSlide = styled(STFlexCont)`
+  display: none;
 
-  background-color: var(--terciary-color);
+  &.${SLIDER_CLASS} {
+    display: block;
+  }
 
   @media screen and (min-width: 765px) {
-    width: 30%;
-    & ~ * {
-      width: 70%;
+    &.${SLIDER_CLASS} {
+      display: flex;
     }
   }
 `;
 
-// const STGrid = styled.div`
-//   width: 90%;
-//   display: grid;
-//   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-//   gap: 1rem;
-//   padding: 1rem;
-//   margin: auto;
-
-//   background-color: var(--main-color);
-// `;
-
-// const STGridItem = styled.article`
-//   background-color: var(--terciary-color);
-
-//   img {
-//     width: 80%;
-//   }
-// `;
-
 export default function Projects() {
+  useEffect(() => {
+    document
+      .getElementById(slidesConfig[0].id)
+      .classList.add(`${SLIDER_CLASS}`);
+  }, []);
+
+  const toggleSlideVisibility = id => {
+    document
+      .querySelectorAll(`.${SLIDER_CLASS}`)
+      .forEach(el => el.classList.remove(`${SLIDER_CLASS}`));
+
+    document.getElementById(id).classList.add(`${SLIDER_CLASS}`);
+  };
+
   return (
     <section id="projects-section">
       <h2>My projects</h2>
       <h3>Projects i've worked in:</h3>
-      <STFlexCont bgCol="var(--main-color)">
-        <STDescription>
-          eaefadaef Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Vero accusantium eaque sunt quam! Nihil, iusto explicabo voluptatum
-          voluptates, libero reiciendis quaerat repudiandae sit autem, deleniti
-          mollitia commodi officia ipsum. Laudantium!
-        </STDescription>
-        <STCarrousel>d</STCarrousel>
-      </STFlexCont>
-      {/* <STGrid>
-        <STGridItem>
-          <img src="vite.svg" alt="" />
-          <h4>Tic tac toe</h4>
-        </STGridItem>
-        <STGridItem>
-          <img src="vite.svg" alt="" />
-          <h4>Tic tac toe</h4>
-        </STGridItem>
-        <STGridItem>
-          <img src="vite.svg" alt="" />
-          <h4>Tic tac toe</h4>
-        </STGridItem>
-      </STGrid> */}
+
+      {slidesConfig.map(({ id, imgUrl, description }) => (
+        <STSlide id={id}>
+          <STPicture>
+            <STImg src={imgUrl} alt="" />
+          </STPicture>
+
+          <STFlexCont>
+            <p>{description}</p>
+          </STFlexCont>
+        </STSlide>
+      ))}
+      <StNav>
+        {slidesConfig.map(({ id }) => (
+          <button onClick={() => toggleSlideVisibility(id)}> &bull;</button>
+        ))}
+      </StNav>
     </section>
   );
 }
