11c51d8c144070d3e3e9f4a184b7c276180b5673
genaroibc <bonavitaismael@gmail.com>
2022-10-18 19:40:32 -0300

    feat: add content to contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,31 +7,51 @@ const STNav = styled.nav`
   justify-content: space-evenly;
   width: 60%;
   margin: auto;
+
+  a {
+    display: flex;
+    gap: 1rem;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 const STHeader = styled.h3`
   font-size: 1.5rem;
 `;
 
+const STSubTitle = styled.h4`
+  font-size: 1.8rem;
+`;
+
+const STPgph = styled.p`
+  font-size: 1.6rem;
+  text-align: center;
+`;
+
 export default function Contact() {
   return (
     <section id="contact">
-      <STSectionTitle>Contact</STSectionTitle>
+      <STSectionTitle>Contact Me</STSectionTitle>
+      <STSubTitle>Let's build something amazing!</STSubTitle>
       <STFlexCont alItems="center" flexDir="column">
         <div>
           <STHeader>You can find me at...</STHeader>
           <STNav>
             <a target="_blank" href="https://github.com/GenaroIBC">
               <Icon title="github" />
+              GitHub
             </a>
             <a target="_blank" href="https://github.com/GenaroIBC">
               <Icon title="github" />
+              LinkedIn
             </a>
           </STNav>
         </div>
         <div>
-          <STHeader>...or also via e-mail</STHeader>
-          <p>correo@gmail.com</p>
+          <STHeader>...or also contact me via e-mail:</STHeader>
+          <STPgph>correo@gmail.com</STPgph>
         </div>
       </STFlexCont>
     </section>
