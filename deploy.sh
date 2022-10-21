9b0a9ecc54ba63920ca282f86aeccde7a36d233b
genaroibc <bonavitaismael@gmail.com>
2022-10-21 14:10:52 -0300

    add gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env sh
 
 # abort on errors
@@ -10,11 +9,14 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -b main
+git checkout -B main
 git add -A
 git commit -m 'deploy'
 
