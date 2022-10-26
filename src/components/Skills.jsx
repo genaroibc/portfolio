30436e448817da709e0d348f1df9b12c0db8cd2a
genaroibc <bonavitaismael@gmail.com>
2022-10-26 11:20:24 -0300

    feat: add soft skills and style icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,58 +6,81 @@ import {
   learning,
 } from "../assets/techStackConfig.json";
 import { v4 as uuid } from "uuid";
+import styled from "styled-components";
+import { Accordion } from "./shared/Accordion";
+import SOFT_SKILLS_DATA from "../assets/softSkills.json";
+
+const STIconListItem = styled.li`
+  display: inline-flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+
+  padding: 1rem;
+  margin: 0.3rem;
+
+  background-color: var(--secondary-color);
+`;
+
+const STTechsList = styled.ul`
+  text-align: center;
+
+  @media screen and (min-width: 992px) {
+    width: 70%;
+    margin: auto;
+  }
+`;
 
 export default function Stack() {
   return (
     <section id="skills">
       <STSectionTitle>Skills</STSectionTitle>
       <STSubTitle>Stack of technologies:</STSubTitle>
-      <STFlexCont jusCont="center">
+      <STFlexCont flexDir="column" jusCont="center">
         <article>
           <STSubTitle fontSize="1.6rem">I know:</STSubTitle>
-
-          {learned.map(el => (
-            <Icon key={uuid()} title={el} />
-          ))}
+          <STTechsList>
+            {learned.map(el => (
+              <STIconListItem key={uuid()}>
+                <Icon title={el} />
+                {el.toUpperCase()}
+              </STIconListItem>
+            ))}
+          </STTechsList>
         </article>
+
         <article>
           <STSubTitle fontSize="1.6rem">I'm learning:</STSubTitle>
 
-          {learning.map(el => (
-            <Icon key={uuid()} title={el} />
-          ))}
+          <STTechsList>
+            {learning.map(el => (
+              <STIconListItem key={uuid()}>
+                <Icon title={el} /> {el.toUpperCase()}
+              </STIconListItem>
+            ))}
+          </STTechsList>
         </article>
+
         <article>
           <STSubTitle fontSize="1.6rem">I'm interested in:</STSubTitle>
 
-          {interestedIn.map(el => (
-            <Icon key={uuid()} title={el} />
-          ))}
+          <STTechsList>
+            {interestedIn.map(el => (
+              <STIconListItem key={uuid()}>
+                <Icon title={el} />
+
+                {el.toUpperCase()}
+              </STIconListItem>
+            ))}
+          </STTechsList>
         </article>
       </STFlexCont>
-      <h3>Soft Skills:</h3>
+
+      <STSubTitle>Soft Skills:</STSubTitle>
 
       <STFlexCont jusCont="center">
         <article>
-          <STSubTitle fontSize="1.6rem">I know:</STSubTitle>
-
-          {learned.map(el => (
-            <Icon key={uuid()} title={el} />
-          ))}
-        </article>
-        <article>
-          <STSubTitle fontSize="1.6rem">I'm learning:</STSubTitle>
-
-          {learning.map(el => (
-            <Icon key={uuid()} title={el} />
-          ))}
-        </article>
-        <article>
-          <STSubTitle fontSize="1.6rem">I'm interested in:</STSubTitle>
-
-          {interestedIn.map(el => (
-            <Icon key={uuid()} title={el} />
-          ))}
+          <Accordion data={SOFT_SKILLS_DATA} />
         </article>
       </STFlexCont>
     </section>
