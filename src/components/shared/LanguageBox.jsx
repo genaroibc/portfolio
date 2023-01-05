2b248d4810ac684f12cab8ea1d8425bfa7597654
genaroibc <bonavitaismael@gmail.com>
2023-01-05 15:30:30 -0300

    dev: add LanguageBox component

---
+++ src/components/shared/LanguageBox.jsx
@@ -0,0 +1,28 @@
+import { changeLanguage } from "i18next";
+import styled from "styled-components";
+
+const STForm = styled.form`
+  select,
+  option {
+    font-size: 1.5rem;
+    background-color: #000;
+    padding: 0.5rem;
+    color: #fffc;
+    border-radius: 5px;
+  }
+
+  option {
+    padding: 1rem;
+  }
+`;
+
+export function LanguageBox() {
+  return (
+    <STForm onChange={e => changeLanguage(e.target.value)}>
+      <select name="lang" id="lang">
+        <option value="es">🇪🇸 Español</option>
+        <option value="en">🇺🇸 English</option>
+      </select>
+    </STForm>
+  );
+}
